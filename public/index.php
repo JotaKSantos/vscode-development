71dded994c2be0e89f3f5780ddccb6e8684c88a9
Jackson dos Santos <jacksondossantos@gmail.com>
2022-07-14 08:36:22 -0300

    feat: alterado toda estrutura

--- public/index.php
+++ public/index.php
@@ -1,8 +1,26 @@
 <?php
-$vars = [
-    10, 20, 30, 44, 55, 66
-];
-$a='s';
-var_dump($vars);
-// echo phpinfo();
-?>
+
+use Migrate\Models\Nfse\Prestador;
+use Migrate\Models\Nfse\Rps;
+
+require(__DIR__.'/../vendor/autoload.php');
+
+$migrate = new Migrate\Client(
+    'SUA_CHAVE_DE_API'
+);
+
+echo ini_get("memory_limit");
+
+$rps = new Rps();
+$rps->RPSNumero = 1;
+$rps->RPSSerie = 0;
+
+$prestador = new Prestador();
+$prestador->CNPJ_prest = '05432831000156';
+$prestador->xName = 'Soft Design Informática LTDA-ME';
+
+$rps->prestador = $prestador;
+
+$nfse = $migrate->nfse()->create($rps);
+
+var_dump($nfse);
